f33ec58e3a75e2c1e6b6c1124196c2cb4a61c997
hitchhubio <hitchhub@gmail.com>
2024-10-12 21:50:25 +0100

    fix: add value docs (#2)

--- packages/token-utils/src/get-value.ts
+++ packages/token-utils/src/get-value.ts
@@ -1,5 +1,15 @@
 import get from 'lodash/get.js';
 
+/**
+ * Retrieves a value from a nested object using a dot-separated path.
+ *
+ * @template T - The type of the value to be returned.
+ * @param {Object} params - The parameters object.
+ * @param {Record<string, unknown>} params.value - The object from which to retrieve the value.
+ * @param {string} params.path - The dot-separated path to the value within the object.
+ * @param {T} [params.defaultValue] - The default value to return if the path does not exist.
+ * @returns {T} - The value found at the specified path, or the default value if the path does not exist.
+ */
 export function getValue<T>({
   value,
   path,
