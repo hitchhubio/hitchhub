b3edd13e32e2d9cef3070f2a58b3ca0560527062
hitchhubio <hitchhub@gmail.com>
2025-11-05 11:48:17 +0000

    wip: update

--- packages/tailwind/src/create-theme.ts
+++ packages/tailwind/src/create-theme.ts
@@ -1,8 +1,9 @@
 import { deepmerge } from '@aaos/deepmerge';
 import { variables } from '@hitchhub/tokens';
 import tailwindTheme from 'tailwindcss/defaultTheme.js';
-import { CustomThemeConfig } from 'tailwindcss/types/config.js';
 import { convertColors, toFont } from './utils.js';
+
+type CustomThemeConfig = Record<string, unknown>;
 
 export type CreateThemeOptions = {
   meta?: any;
@@ -195,16 +196,6 @@ export function createTheme(
       '95': variables.primitive.opacity['95'],
       '100': variables.primitive.opacity['100'],
     },
-    // "boxShadow": {
-    //   "sm": "0 1px 2px 0 rgb(0 0 0 / 0.05)",
-    //   "DEFAULT": "0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)",
-    //   "md": "0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)",
-    //   "lg": "0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1)",
-    //   "xl": "0 20px 25px -5px rgb(0 0 0 / 0.1), 0 8px 10px -6px rgb(0 0 0 / 0.1)",
-    //   "2xl": "0 25px 50px -12px rgb(0 0 0 / 0.25)",
-    //   "inner": "inset 0 2px 4px 0 rgb(0 0 0 / 0.05)",
-    //   "none": "none"
-    // },
     spacing: {
       0: variables.primitive.space['0'],
       none: variables.primitive.space['0'],
