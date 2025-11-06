3e9b0d901136d728081278e423c91bbb23e32efb
hitchhubio <hitchhub@gmail.com>
2025-11-06 20:25:08 +0000

    wip: update

--- packages/react/src/button/button.tsx
+++ packages/react/src/button/button.tsx
@@ -1,7 +1,62 @@
+import { tv } from 'tailwind-variants';
+
+const button = tv({
+  base: 'hitch:p-2 hitch:rounded-lg hitch:text-sm',
+  variants: {
+    variant: {
+      primary: 'hitch:bg-blue-50 hitch:hover:bg-blue-100',
+      secondary: 'hitch:bg-green-100 hitch:hover:bg-green-200',
+    },
+  },
+  defaultVariants: {
+    variant: 'primary',
+  },
+});
+
 export type ButtonProps = {
+  variant?: 'primary' | 'secondary';
   children: React.ReactNode;
+  id?: string;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  formAction?: string | ((formData: FormData) => void | Promise<void>);
+  form?: string;
+  disabled?: boolean;
+  invalid?: boolean;
+  ref?: React.Ref<HTMLButtonElement>;
+  type?: 'button' | 'submit' | 'reset';
+  ariaDescribedBy?: string;
+  ariaLabelledBy?: string;
 };
 
-export function Button({ children }: ButtonProps) {
-  return <p className="hitch:bg-blue-500">{children}</p>;
+export function Button({
+  id,
+  onClick,
+  formAction,
+  form,
+  variant,
+  children,
+  disabled,
+  invalid,
+  ref,
+  type = 'button',
+  ariaDescribedBy,
+  ariaLabelledBy,
+}: ButtonProps) {
+  return (
+    <button
+      id={id}
+      onClick={onClick}
+      formAction={formAction}
+      form={form}
+      className={button({ variant })}
+      ref={ref}
+      disabled={disabled}
+      type={type}
+      aria-labelledby={ariaLabelledBy}
+      aria-invalid={invalid}
+      aria-describedby={ariaDescribedBy}
+    >
+      {children}
+    </button>
+  );
 }
